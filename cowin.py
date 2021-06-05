52558ddb416a50e0eb96761473a2c386befbd478
Atharva21 <atharva.m.mulay@gmail.com>
2021-06-05 16:34:38 +0530

    resources file

--- cowin.py
+++ cowin.py
@@ -1,8 +1,21 @@
-import requests, yaml
+import datetime, yaml, requests
 
+class Data:
+	prop_file = './resources/application.yml'
+	
+	def __init__(self):
+		pass
+	
+	def validate(self):
+		pass
+
+class WebClient:
+	pass
+		
 
 def run():
-	pass
+	data = Data()
+	today = '-'.join(str(datetime.datetime.now()).split(' ')[0].split('-')[::-1])
 
 if __name__ == "__main__":
 	run()
